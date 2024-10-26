38d65cae449841d9a512210a36d24b8785ab52b7
Ömer <omerkaya.1995@gmail.com>
2024-10-26 19:24:37 +0300

    initial commit

---
+++ src/main/java/com/example/livebettingapp/LiveBettingAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.livebettingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveBettingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveBettingAppApplication.class, args);
+    }
+
+}
+ 
